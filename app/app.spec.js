789ea1c14ad634d22c43d9551aecb3228fa325d7
Anthony K <antonyk@users.noreply.github.com>
2020-06-24 15:46:34 -0400

    tests: auth endpoints

--- app/app.spec.js
+++ app/app.spec.js
@@ -1,9 +1,12 @@
+const vars = require('../vars')
+// vars.APP_ENV = "testing"
 const supertest = require('supertest')
 const server = require('../index')
 
-
 // auth
 describe("app", () => {
+
+
 
   const uname = `u${Math.random()}`
   const pass = "I am secret"
@@ -13,31 +16,65 @@ describe("app", () => {
   })
 
   describe("/auth/register", () => {
-    // it("returns error when invalid data provided", () => {
-    //   return supertest(server)
-    //     .post("/api/auth/register")
-    //     .send({username: uname, password: pass})
-    //     .then(response => {
-    //       expect(response.status).toBe(501)
-    //     })
-    // })
+    it("returns error when invalid data provided", () => {
+      return supertest(server)
+        .post("/auth/register")
+        .send({ password: pass })
+        .then(response => {
+          expect(response.status).toBe(400)
+        })
+    })
 
-    it.todo("returns success when good data provided")
+    it("returns success when good data provided", () => {
+      return supertest(server)
+        .post("/auth/register")
+        .send({username: uname, password: pass})
+        .then(response => {
+          expect(response.status).toBe(200)
+        })
+    })
 
-    it.todo("returns error when trying a duplicate username")
+    it("returns error when trying a duplicate username", () => {
+      return supertest(server)
+        .post("/auth/register")
+        .send({username: uname, password: pass})
+        .then(response => {
+          expect(response.status).toBe(500)
+        })
+    })
 
   })
 
   describe("/auth/login", () => {
-    it.todo("")
+
+    it("returns error when invalid data provided", () => {
+      return supertest(server)
+        .post("/auth/login")
+        .send({ username: uname })
+        .then(response => {
+          expect(response.status).toBe(400)
+        })
+    })
+
+    it("returns error when trying a duplicate username", () => {
+      return supertest(server)
+        .post("/auth/register")
+        .send({username: uname, password: pass})
+        .then(response => {
+          expect(response.status).toBe(500)
+        })
+    })
+
+    it("returns success when good data provided", () => {
+      return supertest(server)
+        .post("/auth/register")
+        .send({username: uname, password: pass})
+        .then(response => {
+          expect(response.status).toBe(200)
+        })
+    })
+
   })
-
-
-
-
-
-
 
 })
 
-// app
